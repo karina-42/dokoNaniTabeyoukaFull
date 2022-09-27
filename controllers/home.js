658e6999d77e50263e183a11d9c7383147a77168
Angela Vegega <angela.vegega@gmail.com>
2022-09-27 21:05:18 +0900

    Added mongoose model and dropdown list in index.ejs with food values

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,24 @@
+const { Decimal128 } = require("mongodb")
+const Restaurant = require("../models/Restaurant")
+
 module.exports = {
-  getIndex: (req,res)=>{
-      res.render('index.ejs')
+    getIndex: async (req, res)=>{
+    try {
+      //get Restaurant data
+      const data = await Restaurant.find({})
+      //Make an array of the food keys
+      let foodKeys = []
+      for(let document of data) {
+        foodKeys.push(document.food)
+      }
+      //Filter the food keys so they don't repeat in the dropdown list
+      let uniqueFood = foodKeys.filter((value, index, arr) => {
+         return arr.indexOf(value) === index
+      })
+      //render the homepage and send the unique food keys array under the variable dropdownValues
+      res.render('index.ejs', {dropdownValues: uniqueFood})
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const methodOverride = require("method-override")
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 
-
+//Use .env file in config folder
 require('dotenv').config({path: './config/.env'})
 
+//Connect To Database
 connectDB()
 
+//Using EJS for views
 app.set('view engine', 'ejs')
+
+//Static Folder
 app.use(express.static('public'))
+
+//Body Parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+//Use forms for put / delete
+app.use(methodOverride("_method"))
+
+//Setup Routes For Which The Server Is Listening
 app.use('/', mainRoutes)
 
-
+//Server Running
 app.listen(process.env.PORT, ()=>{
   console.log('Server is running, you better catch it!')
 })  
