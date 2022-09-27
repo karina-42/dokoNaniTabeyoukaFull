8b01680be61a66ce77e088356c01cfb7a6950f08
Angela Vegega <angela.vegega@gmail.com>
2022-09-27 18:14:10 +0900

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', mainRoutes)
+
+
+app.listen(process.env.PORT, ()=>{
+  console.log('Server is running, you better catch it!')
+})  
